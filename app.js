243b9fc724d208578c77ed717b6d3d2d447bfa1f
AB Noori <ab@ABs-MacBook-Pro.local>
2020-11-28 02:06:49 -0800

    Users can create new channels and join

--- app.js
+++ app.js
@@ -41,11 +41,21 @@ server.listen('3000', () => {
 app.use('/public', express.static('public'))
 
 
+// const io = require('socket.io')(server);
+// //We'll store our online users here
+// let onlineUsers = {};
+// io.on("connection", (socket) => {
+//   // Make sure to send the users to our chat file
+//   require('./sockets/chat.js')(io, socket, onlineUsers);
+})
+
 const io = require('socket.io')(server);
-//We'll store our online users here
 let onlineUsers = {};
+//Save the channels in this object.
+let channels = {"General" : []}
+
 io.on("connection", (socket) => {
-  // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers);
+  // Make sure to send the channels to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
 })
 
